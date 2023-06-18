705373b4884c8f61a341772fc3692a8d4454cb8c
Yavor Petkov <yavorp.98@gmail.com>
2023-06-18 22:53:44 +0300

    Add detail page

--- law-list.js
+++ law-list.js
@@ -4,3 +4,18 @@ const SEARCH_KEY_ID = 'searchKey';
 const workWeekSelection = document.getElementById(SEARCH_KEY_ID);
 const searchValue = window.localStorage.getItem(SEARCH_BY_KEY);
 workWeekSelection.textContent = searchValue;
+
+const rotuerBtn = document.getElementById('normal-work-hours-rotuer-btn');
+rotuerBtn && rotuerBtn.addEventListener('click', () => {
+    window.location.href = 'detail.html';
+});
+
+const backBtn = document.getElementById('detail-back-btn');
+backBtn?.addEventListener('click', () => {
+    window.history.back();
+});
+
+const saveBtn = document.getElementById('detail-save-btn');
+saveBtn?.addEventListener('click', () => {
+    window.location.href = 'saved.html';
+})
